298069c8fea1ce633f1cf552a7deecc825927340
Robert.minia <omid-bakeri>
2023-09-13 23:41:23 +0330

    file updated -- supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,25 @@
+import {useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import {getCabins} from "../services/apiCabins.js";
+
 
 function Cabins() {
+    // const [dataFetch , setDataFetch] = useState();
+
+    useEffect(() => {
+        const fetchData = async ()=>{
+            const res = await getCabins();
+            console.log(res)
+        }
+
+        fetchData();
+    }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
+
       <p>TEST</p>
     </Row>
   );
